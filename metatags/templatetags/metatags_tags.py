64398b877ac2a62d5b32ed483f162a76592c585e
Agustín Méndez <matagus@gmail.com>
2012-09-02 00:02:07 -0300

    Simplifying templatetags.
     * simpler cache logic.
     * removing dependency on ContentType.

--- metatags/templatetags/metatags_tags.py
+++ metatags/templatetags/metatags_tags.py
@@ -1,5 +1,5 @@
+# -*- coding: UTF-8 -*-
 from django import template
-from django.contrib.contenttypes.models import ContentType
 from django.core.cache import cache
 
 from metatags import app_settings, models
@@ -9,23 +9,26 @@ register = template.Library()
 @register.inclusion_tag('metatags/meta_tags.html')
 def meta(obj=None, meta={}):
     """
-    Template tag to generate meta tags. Takes an optional parameter of a 
-    template object to pull the tags from. If not passed, it will return the 
+    Template tag to generate meta tags. Takes an optional parameter of a
+    template object to pull the tags from. If not passed, it will return the
     default meta tags, as set in their respective templates.
     """
-    cachename = obj and 'metatags_%s_%s' % (
-        obj.__class__.__name__,
-        obj.id
-    ) or 'meta_default'
-    meta_tags = cache.get(cachename)
-    if not meta_tags:
+    try:
+        cachename = 'metatags_%s_%s' % (obj.__class__.__name__, obj.id)
+    except AttributeError:
+        cachename = 'meta_default'
+
+    metatags = cache.get(cachename)
+
+    if not metatags and cachename != 'meta_default':
         try:
-            meta_tags = MetaTag.objects.get(
-                content_type=ContentType.objects.get_for_model(obj.__class__),
-                object_id=obj.pk
-            )
-        except (AttributeError, IndexError):
-            meta_tags = None
+            metatags = models.MetaTag.objects.get(object_id=obj.id,
+                content_type__app_label=obj._meta.app_label,
+                content_type__model=obj._meta.module_name)
+        except models.MetaTag.DoesNotExist:
+            metatags = None
+
         if app_settings.METATAGS_CACHE_TTL:
-            cache.set(cachename, meta_tags, app_settings.METATAGS_CACHE_TTL)
-    return { 'meta_tags': meta }
+            cache.set(cachename, metatags, app_settings.METATAGS_CACHE_TTL)
+
+    return {"meta_tags": metatags}
